40f45b3883da105d1b44894833281ebcab59be26
Rosemarries <rosemarries.404notfound@gmail.com>
2022-10-25 21:57:02 +0700

    add web

---
+++ web/app.py
@@ -0,0 +1,12 @@
+from flask import Flask
+from views import views
+
+app = Flask(__name__)
+app.register_blueprint(views, url_prefix="/views")
+
+@app.route("/")
+def home():
+    return "This is the home page"
+
+if __name__ == "__main__":
+    app.run(debug=True, port=5555)
---
+++ web/views.py
@@ -0,0 +1,20 @@
+from flask import Blueprint, render_template, request, jsonify, redirect, url_for
+import cgi
+
+views = Blueprint(__name__, "views")
+
+@views.route("/")
+def home():
+    return render_template("index.html")
+
+@views.route("/result", methods = [ "POST", "GET" ])
+def result():
+    # need to fix this one!!!
+    if request.method == "GET":
+        from actions import word_l
+        form = cgi.FieldStorage()
+        word_input = form.getvalue("word")
+        if word_input in word_l:
+            return f"We have {word_input} in our dictionary!"
+        from actions import word_summary, word_matrixs
+        return render_template("result.html", result = [word_summary, word_matrixs])
